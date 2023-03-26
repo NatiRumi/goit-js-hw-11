b52796fd9f3c81a42185fc111f29a36435e6962e
NatiRumi <NatiRumiantseva@gmail.com>
2023-03-26 22:18:51 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,36 +1,69 @@
-
 import axios from "axios";
-
-
+import Notiflix from 'notiflix';
+// import { getImage } from "../src/js/requestAPI";
 
 const searchBtn = document.querySelector('#search-form');
-const searchValue = document.querySelector('input').value.trim();
+
 
 searchBtn.addEventListener('submit', event => {
     event.preventDefault();
 
-    getImage(searchValue);
-    
+    const searchValue = document.querySelector('input').value.trim();
+
+    getImage(searchValue)
+    .then((responce) => {console.log(response)})
+    .catch((error) => console.error(error)); 
 })
 
 
-const getImage  = (search) => {
-    // const searchParams = new URLSearchParams({
-    //     key: '34747655-3d476a5c24b1ab5d2173a79ca',
-    //     q: document.querySelector('input').value.trim(),
-    //     image_type: "photo",
-    //     orientation: "horizontal",
-    //     safesearch: "true"
-    // })
+function getImage(search) {
+    const searchParams = new URLSearchParams({
+        key: '34747655-3d476a5c24b1ab5d2173a79ca',
+        q: search,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: "true"
+    })
 
-    // return fetch('https://pixabay.com/api/?key=34747655-3d476a5c24b1ab5d2173a79ca&q=searchValue&image_type=photo').then(responce => {
-    // console.log(responce);    
-    // return responce.json();
-    // }).catch()
-    
     axios
-    .get('https://pixabay.com/api/?key=34747655-3d476a5c24b1ab5d2173a79ca&q=searchValue&image_type=photo')
-    .then((response) => {console.log(response)})
+    .get(`https://pixabay.com/api/?${searchParams.toString()}`)
+    .then((response) => {
+        if(response.data.hits == 0){
+            Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+            return;
+        };
+        // console.log(response.data.hits);
+        return response.data.hits;
+    })
     .catch((error) => console.error(error))
+};
+
+
+
+function renderCollection(collection) {
+    const galleryBox = document.querySelector('.gallery');
+    galleryBox.innerHTML = '';
+    const images = collection.map(item => {
+        return `<div class="photo-card">
+        <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b> ${item.likes}
+          </p>
+          <p class="info-item">
+            <b>Views</b>  ${item.views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b>  ${item.comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b> ${item.downloads}
+          </p>
+        </div>
+      </div>`
+    }).join("");
+    galleryBox.insertAdjacentHTML('beforeend', images);
 }
+
+
 
---
+++ src/js/requestAPI.js
@@ -0,0 +1,24 @@
+// import axios from "axios";
+
+// function getImage (search) {
+//     const searchParams = new URLSearchParams({
+//         key: '34747655-3d476a5c24b1ab5d2173a79ca',
+//         q: search,
+//         image_type: "photo",
+//         orientation: "horizontal",
+//         safesearch: "true"
+//     })
+
+//     axios
+//     .get(`https://pixabay.com/api/?${searchParams.toString()}`)
+//     .then((response) => {
+//         // if(!response.ok){
+//         //     throw new Error(response.status);
+//         // };
+//         console.log(response.data)})
+//     .catch((error) => console.error(error))
+// };
+
+// export { getImage }
+
+
